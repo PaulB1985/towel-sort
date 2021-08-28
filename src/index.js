ee7a811e0687c95ee035d3ebe0b254a3c4330ecf
PaulB1985 <paulpriest1985@gmail.com>
2021-08-28 13:41:22 +0300

    feat: add towel sort code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    let combined = []
+    for (m in matrix) {
+        let units = []
+        if (m % 2 == 0) {
+            let unit = [...matrix[m]]
+            units.push(...unit)
+        } else {
+            let unit = [...matrix[m]].reverse()
+            units.push(...unit)
+        }
+        combined.push(...units)
+    }
+    return combined
 }
